0b79a52ea917fbf42dbd1a6421852aa869b40bf4
Adrian Agnic <adrian@tura.io>
2018-04-30 23:09:33 -0700

    start client script

---
+++ nodes/s.py
@@ -0,0 +1,28 @@
+import socket
+import threading
+import sys
+
+__author__ = 'Adrian Agnic'
+__version__ = '0.0.1'
+
+REM_HOST = sys.argv[1]
+REM_PORT = sys.argv[2]
+S_SCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+S_SCK.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+def response_handler():
+    while True:
+        msg = S_SCK.recv(1024)
+        print(f"::MSG:: {msg.decode('utf-8')}")
+
+def chat():
+    S_SCK.connect((str(REM_HOST), int(REM_PORT)))
+    t = threading.Thread(target=response_handler)
+    t.start()
+    while True:
+        msg = input()
+        S_SCK.send(msg.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    chat()
