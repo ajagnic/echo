a7e306b2d663b3a87975c7f856de8a664009d10b
Adrian Agnic <adrian@tura.io>
2018-05-20 00:38:46 -0700

    server tdd

--- chat/mserver.py
+++ chat/mserver.py
@@ -1,29 +1,69 @@
+''' tbd '''
+
 __author__ = 'Adrian Agnic'
 __version__ = '0.0.1'
 
 import socket
 import select
+import queue
 
+_server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+_server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+_server_sock.setblocking(False)
+_server_sock.bind(('127.0.0.1', 8888))
 
-_host = '127.0.0.1'
-_port = 8888
+_i = [_server_sock]
+_o = []
 _message_pipeline = {}
-_input = []
-_output = []
 
-_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-_server_socket.setblocking(False)
-_server_socket.bind((str(_host), int(_port)))
-_input.append(_server_socket)
+
+def remove_client(sock):
+    try:
+        _i.remove(sock)
+        _o.remove(sock)
+        sock.close()
+    except:
+        pass
+    del _message_pipeline[sock]
 
 
 def main():
-    _server_socket.listen(5)
+    _server_sock.listen(5)
+    while _i:
+        incoming_data, open_buffers, bad_socks = select.select(_i, _o, _i)
+        for sock in incoming_data:
+            if sock is _server_sock:
+                # new connection pending
+                new_client, addr = sock.accept()
+                new_client.setblocking(False)
+                _i.append(new_client)
+                _message_pipeline[new_client] = queue.Queue()
+            else:
+                # incoming message data
+                new_msg = sock.recv(2048)
+                if new_msg:
+                    for client in _message_pipeline.keys():
+                        if client not in _o:
+                            _o.append(client)
+                        if client is not sock:
+                            _message_pipeline[client].put(new_msg)
+                else:
+                    # closed connection
+                    remove_client(sock)
+        for sock in open_buffers:
+            # progress pipeline
+            try:
+                queued_msg = _message_pipeline[sock].get_nowait()
+            except queue.Empty:
+                _o.remove(sock)
+            else:
+                sock.send(queued_msg)
+        for sock in bad_socks:
+            remove_client(sock)
 
 
 
 if __name__ == '__main__':
     main()
 else:
-    _server_socket.close()
+    _server_sock.close()
---
+++ chat/test/test_mserver.py
@@ -0,0 +1,26 @@
+''' tests require Mserver running separately '''
+
+import unittest
+import socket
+
+
+class test_mserver(unittest.TestCase):
+    def setUp(self):
+        self.test_socket = socket.socket()
+    
+    def test_connect(self):
+        self.test_socket.connect(('127.0.0.1', 8888))
+
+    def test_message_pipeline(self):
+        producer_socket = socket.socket()
+        producer_socket.connect(('127.0.0.1', 8888))
+        self.test_connect()
+        producer_socket.send("test".encode())
+        received_msg = None
+        while received_msg is None:
+            received_msg = self.test_socket.recv(1024)
+        self.assertEqual('test', received_msg.decode())
+        producer_socket.close()
+
+    def tearDown(self):
+        self.test_socket.close()
